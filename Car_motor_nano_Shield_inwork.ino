7d0cc978ccc786bd18650b1a082bfb217b388f66
YigalB <yigalb@hotmail.com>
2018-09-21 21:40:08 +0300

    Change pins nums to defines

--- Car_motor_nano_Shield_inwork.ino
+++ Car_motor_nano_Shield_inwork.ino
@@ -2,7 +2,7 @@
 Motor Shield 1-Channel DC Motor Demo
 http://www.instructables.com/id/Arduino-Motor-Shield-Tutorial/
 *************************************************************/
-#include <Servo.h> 
+#include <Servo.h>
 
 #define MOTOR_LEFT_DIR_PIN 12
 #define MOTOR_LEFT_BREAK_PIN 9
@@ -20,7 +20,7 @@ http://www.instructables.com/id/Arduino-Motor-Shield-Tutorial/
 #define MOTOR_RIGHT_MAX_SPEED 255 // max is 255. should be lower if calibrated needed
 
 #define NANO 0 // make 0 in case of UNO; 1 in case of Nano
-#define UNo  1 
+#define UNo  1
 
 #define DEBUG 1
 #define CALIBRATE 1
@@ -49,32 +49,32 @@ Servo myservo;  // create servo object to control a servo
 //******************* SETUP ************************
 
 void setup() {
-  
+
   //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(MOTOR_LEFT_DIR_PIN, OUTPUT); //Initiates Motor Channel A pin
   pinMode(9, OUTPUT); //Initiates Brake Channel A pin
   //Setup Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(MOTOR_RIGHT_DIR_PIN, OUTPUT); //Initiates Motor Channel A pin
   pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
   // UltrSonic sensor
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT); 
-  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
   #if 1==NANO
     myservo.attach(7);  // Use pin 7 in case of NANO
   #else
     myservo.attach(5);  // Use pin 5 in case of UNO
   #endif
-  
+
   #ifdef DEBUG
     Serial.begin(9600);
   #endif
-  
+
   #ifdef CALIBRATE
     Calibrate_wheels();
   #endif
-  
-  
+
+
   // test motors
   #ifdef TEST_MODE
     // testing: making sure car parts are well
@@ -87,31 +87,31 @@ void setup() {
       delay(10000000); // wait for ever, so I could debug the results
     }
   #endif
-  
+
 }
 
 
 //******************* LOOP ************************
 void loop(){
   int dir,dist,tmp;
-  
+
   stop();
   dist = scan();
-  
+
    for (tmp = 0; tmp < SERVO_STEPS_NUM; tmp += 1) {
     // TBD array need to be SERVO_STEPS_NUM+1
     // enter for statment to ifdef
-    
+
     #ifdef DEBUG
     Serial.print(dist_array[tmp]);
     Serial.print(" ");
-    #endif 
+    #endif
    }
-  
-  
+
+
   dir=decide(dist);
-  
-  
+
+
   #ifdef DEBUG
     Serial.println(" ");
     Serial.print("dist= ");
@@ -120,9 +120,9 @@ void loop(){
     Serial.println(dir);
     Serial.print("steps inc= ");
     Serial.println(SERVO_STEPS_INC);
-  #endif 
-  
-  
+  #endif
+
+
   if (FORWARD == dir)
     go_forward();
   else {
@@ -130,7 +130,7 @@ void loop(){
     delay(1000);
     go_backward();
   }
-  
+
   /*
   if (BACKWARD==dir)
     go_backward();
@@ -139,8 +139,8 @@ void loop(){
   if (RIGHT==dir)
     go_right();
   */
-  
-  
+
+
   delay(3000);
 
 
@@ -153,37 +153,37 @@ void loop(){
   digitalWrite(8, LOW);   //Disengage the Brake for Channel B
   analogWrite(11, 123);    //Spins the motor on Channel B at half speed
 */
-  
+
 //  delay(3000);
 
-  
+
 //  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
 //  digitalWrite(9, HIGH);  //Engage the Brake for Channel B
 
 
 //  delay(1000);
-  
-  
+
+
   //Motor A forward @ full speed
 //  digitalWrite(12, LOW);  //Establishes backward direction of Channel A
 //  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
 //  analogWrite(3, 123);    //Spins the motor on Channel A at half speed
-  
+
   //Motor B forward @ full speed
 //  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
 //  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
 //  analogWrite(11, 255);   //Spins the motor on Channel B at full speed
-  
-  
+
+
 //  delay(3000);
-  
-  
+
+
 //  digitalWrite(MOTOR_LEFT_BREAK_PIN, HIGH);  //Engage the Brake for Channel A
 //  digitalWrite(MOTOR_RIGHT_BREAK_PIN, HIGH);  //Engage the Brake for Channel B
-  
-  
+
+
 //  delay(1000);
-  
+
 }
 
 
@@ -231,7 +231,7 @@ void motor_go(int l_side, int l_dir_pin, int l_break_pin, int l_speed)
     Serial.println(spd_pin);
     Serial.println("~~~~~~~~~~~~~~~~~~~ ");
   #endif
-  
+
   digitalWrite(left_or_right, HIGH); //Establishes forward direction of Channel A
   digitalWrite(brk_pin, LOW);   //Disengage the Brake for Channel A
   analogWrite(spd_pin, l_speed);   //Spins the motor on Channel A at full speed
@@ -241,7 +241,7 @@ void motor_go(int l_side, int l_dir_pin, int l_break_pin, int l_speed)
 
 void go_forward()
 {
-  
+
   #ifdef DEBUG
     Serial.println("in go_forward");
   //  Serial.print(MOTOR_LEFT_DIR_PIN);
@@ -249,14 +249,14 @@ void go_forward()
   //  Serial.print(MOTOR_LEFT_BREAK_PIN);
   //  Serial.print(" ");
   //  Serial.println(MOTOR_LEFT_SPEED_PIN);
-  #endif 
+  #endif
 
   //Motor A forward @ full speed
 
   digitalWrite(MOTOR_LEFT_DIR_PIN, HIGH); //Establishes forward direction of Channel A
   digitalWrite(MOTOR_LEFT_BREAK_PIN, LOW);   //Disengage the Brake for Channel A
   analogWrite(MOTOR_LEFT_SPEED_PIN, motor_left_speed);   //Spins the motor on Channel A at full speed
-  
+
   //Motor B forward @ full speed
   digitalWrite(MOTOR_RIGHT_DIR_PIN, HIGH); //Establishes forward direction of Channel A
   digitalWrite(MOTOR_RIGHT_BREAK_PIN, LOW);   //Disengage the Brake for Channel A
@@ -266,13 +266,13 @@ void go_forward()
 
 void go_backward()
 {
-  
+
 }
 
 
 void go_right()
 {
-  
+
 }
 
 
@@ -282,7 +282,7 @@ void go_left()
   digitalWrite(MOTOR_LEFT_DIR_PIN, HIGH); //Establishes forward direction of Channel A
   digitalWrite(MOTOR_LEFT_BREAK_PIN, LOW);   //Disengage the Brake for Channel A
   analogWrite(MOTOR_LEFT_SPEED_PIN, 255);   //Spins the motor on Channel A at full speed
-  
+
   //Motor B forward @ half speed
   digitalWrite(MOTOR_RIGHT_DIR_PIN, HIGH); //Establishes forward direction of Channel A
   digitalWrite(MOTOR_RIGHT_BREAK_PIN, LOW);   //Disengage the Brake for Channel A
@@ -291,12 +291,12 @@ void go_left()
 
 
 void  stop() {
-  
+
   #ifdef DEBUG
     Serial.println("in stop");
-  #endif 
-  
-  
+  #endif
+
+
   digitalWrite(MOTOR_LEFT_BREAK_PIN, HIGH);  //Engage the Brake for Channel A
   digitalWrite(MOTOR_RIGHT_BREAK_PIN, HIGH);  //Engage the Brake for Channel B
 }
@@ -304,43 +304,43 @@ void  stop() {
 
 int  scan() {
   int k=0,pos,read_dist,cnt;
-  
-  
+
+
   #ifdef DEBUG
     Serial.println("in scan");
-  #endif 
-  
-  
+  #endif
+
+
   for (pos = 0; pos < SERVO_STEPS_NUM; pos += 1) {
   //for (pos = 0; pos <= 180; pos += 10) { // from 0 degrees to 180 degrees in steps of 10 degree
     myservo.write(pos * SERVO_STEPS_INC);              // tell servo to go to position in variable 'pos'
     delay(500);                       // waits 15ms for the servo to reach the position
-    
+
     read_dist = readDistance();
-    
+
     dist_array[pos] = read_dist;
-    
+
 // TBD read into array using k variable
 // currently - return something
   #ifdef DEBUG
     Serial.print("distance: ");
     Serial.println(read_dist);
-  #endif 
+  #endif
 
   }
-  
+
   myservo.write(90); // bring to center
-  return readDistance();    // TBD - tmp untill array is analysed t!!!  
+  return readDistance();    // TBD - tmp untill array is analysed t!!!
 }
 
 
 int  decide(int l_dist) {
   int decision;
-  
+
   #ifdef DEBUG
     Serial.print("in decide.");
-  #endif 
-  
+  #endif
+
   if (decide_if_forward())
   	decision = FORWARD;
   else {
@@ -348,13 +348,13 @@ int  decide(int l_dist) {
   	delay(300);
   	decision = BACKWARD;
   }
-    
+
   return decision;
 }
 
 
-// will decide if the car can go forward. 
-// if can't - returns 0 
+// will decide if the car can go forward.
+// if can't - returns 0
 // if can - return any other
 int decide_if_forward() {
   int i;
@@ -363,20 +363,20 @@ int decide_if_forward() {
   for (i = 0 ; i < SERVO_STEPS_NUM ; ++i) {
   	min_dist = min_dist > dist_array[i] ? dist_array[i] : min_dist;
   }
-  
+
   if (STOP_RANGE >= min_dist)
   	return 0; // STOP. don't go forward
-  
+
   return 1; // able to go forward
 }
- 
+
 // void  go(){}
 
 
 int readDistance() {
-  
+
   int dist_t,duration_t;
-  
+
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -392,8 +392,8 @@ int readDistance() {
 
   // Calculating the distance
   dist_t = duration_t*0.034/2;
-  
-  
+
+
   #ifdef DEBUG
    //Serial.print("in read distance: ");
    //Serial.print("trigPin: ");
@@ -404,8 +404,8 @@ int readDistance() {
    //Serial.print(duration_t);
    //Serial.print("dist_t= ");
    //Serial.println(dist_t);
-  #endif 
-  
+  #endif
+
   return(dist_t);
 }
 
@@ -413,7 +413,7 @@ int readDistance() {
 void test_motors(){
     // void because of human validatio
     // maybe add hardware in the future to measure wheels movements
-    
+
     Serial.println("**** Testing Motors");
     Serial.println("----Start LEFT,slow speed");
     motor_go(LEFT,HIGH,LOW, MOTOR_LEFT_MAX_SPEED/2);
@@ -430,14 +430,14 @@ void test_motors(){
     motor_go(RIGHT,HIGH,LOW, MOTOR_RIGHT_MAX_SPEED);
     delay (2000);
     stop();
-    
+
     Serial.println("**** end testing motors");
-  
+
 }
 
 
 void servo_test() {
-  
+
   #ifdef DEBUG
     Serial.println("***** testing servo ");
   #endif
@@ -467,23 +467,23 @@ void ultrasonic_test() {
 
 void Calibrate_wheels() {
   // Calibrating the wheels, since some engines may run faster than others
-  // How: 
+  // How:
   // HW: add potentiometer to allow extranal human input interface, keep connexted to laptop USB (?)
-  // SW: 
+  // SW:
   // use CALIBRATE to do it once
-  // 1 - let the car go for 2 seconds, then changing 
-  // 2 - define whicj wheel to claibrate (#define WHEEL). 
+  // 1 - let the car go for 2 seconds, then changing
+  // 2 - define whicj wheel to claibrate (#define WHEEL).
   //      slow down the faster one
-  // 2 - wait 5 seconds to allow human change potentiometer 
+  // 2 - wait 5 seconds to allow human change potentiometer
   // 3 - back to (1) untill car goes straight
   // 4 - copy the value from the monitor and save it
-  
+
     int calib_in; // value from calibration potentiometer
 
     Serial.print("**** Calibrating wheels");
     go_forward();
     delay(2000);
-    
+
     while(true){
       // endless loop to allow calibration
       // once clibrated, unset CALIBRATE and re-start
@@ -494,7 +494,7 @@ void Calibrate_wheels() {
       set_wheel_speed(LEFT,calib_in);   // TBD - currently left wheel needs to be slower
       go_forward();
     }
-  
+
 }
 
 
@@ -504,11 +504,11 @@ int read_calibrate_input()
   // done only in calibration mode in setup
   int val = analogRead(analog_in_pin);              // input value 0..1023
   int val1 = map(val, 0, 1023, 0, 255);  // mapped value to 0..255
-  
+
   Serial.print(val);
   Serial.print(" mapped to 0..255 value: ");
   Serial.println(val1);
-  return val1; 
+  return val1;
 }
 
 
@@ -519,11 +519,10 @@ void set_wheel_speed(int l_wheel,int l_wheel_speed)
   Serial.print(l_wheel);
   Serial.print(" Speed " );
   Serial.println(l_wheel_speed);
-  
+
   if (LEFT == l_wheel)
     motor_left_speed = l_wheel_speed;
   else
     motor_right_speed = l_wheel_speed;
-  
-}
 
+}
