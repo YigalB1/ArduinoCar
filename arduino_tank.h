056ae4caab550b7857d101e91e41ced006e294d1
YigalB <yigalb@hotmail.com>
2018-11-13 14:18:01 +0200

    Update arduino_tank.h

--- arduino_tank.h
+++ arduino_tank.h
@@ -1,2 +1,7 @@
 // Arduino Tank header
 
+// links to tank assembly:
+//  https://github.com/SmartArduino/XPT/blob/master/SR14_English.pdf
+//  https://github.com/SmartArduino/SZDOITWiKi/wiki/T101-tank-chassis
+//  https://www.youtube.com/watch?v=vy1ScmWc-P4&feature=youtu.be
+//  https://github.com/SmartArduino/SZDOITWiKi/wiki/Robot--Assembling-Chassis
